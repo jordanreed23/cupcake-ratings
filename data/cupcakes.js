c37cccca70d0e4c06aad3ecbced1c73e66f04fd3
Brooks Patton <brooks.patton@galvanize.com>
2017-10-04 11:42:11 -0600

    added link to cupcakes, and removed link styles

--- data/cupcakes.js
+++ data/cupcakes.js
@@ -1,17 +1,20 @@
 const cupcakes = [
 	{
+		id: 0,
 		name: 'Red Velvet',
 		ratings: [3, 1, 5, 4],
 		baker: 'Peter',
 		averageRating: (3 + 1 + 5 + 4) / 4
 	},
 	{
+		id: 1,
 		name: 'Chocolate',
 		ratings: [4, 5, 5, 4],
 		baker: 'Michelle',
 		averageRating: (4 + 5 + 5 + 4) / 4
 	},
 	{
+		id: 2,
 		name: 'Vanilla',
 		ratings: [4, 4, 5, 4],
 		baker: 'Jess',
