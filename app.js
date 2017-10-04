f06800c813a93df6bc92478e0056ec4c51413f8b
Brooks Patton <brooks.patton@galvanize.com>
2017-10-04 10:42:10 -0600

    cupcakes can be viewed, unstyled

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const cupcakes = require('./data/cupcakes');
 
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+	res.render('index', {
+		title: 'cupcake reviews!',
+		cupcakes: cupcakes
+	});
+});
 
 app.listen(port, () => {
 	console.log('server listening on port', port);
---
+++ data/cupcakes.js
@@ -0,0 +1,22 @@
+const cupcakes = [
+	{
+		name: 'Red Velvet',
+		ratings: [3, 1, 5, 4],
+		baker: 'Peter',
+		averageRating: (3 + 1 + 5 + 4) / 4
+	},
+	{
+		name: 'Chocolate',
+		ratings: [4, 5, 5, 4],
+		baker: 'Michelle',
+		averageRating: (4 + 5 + 5 + 4) / 4
+	},
+	{
+		name: 'Vanilla',
+		ratings: [4, 4, 5, 4],
+		baker: 'Jess',
+		averageRating: (4 + 4 + 5 + 4) / 4
+	}
+];
+
+module.exports = cupcakes;
